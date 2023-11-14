720821ba13948b633aea8f4fd8691e6e03c78600
xXSchrandXx <marvinschrand@yahoo.de>
2023-11-14 20:16:02 +0100

    Add attachment permission check

--- files/lib/system/attachment/AssetAttachmentObjectType.class.php
+++ files/lib/system/attachment/AssetAttachmentObjectType.class.php
@@ -2,25 +2,33 @@
 
 namespace assets\system\attachment;
 
+use assets\data\asset\Asset;
+use assets\data\asset\AssetList;
 use wcf\system\attachment\AbstractAttachmentObjectType;
 
+/**
+ * @var Asset[] $cachedObjects
+ * @method Asset getObject($objectID)
+ */
 class AssetAttachmentObjectType extends AbstractAttachmentObjectType
 {
     /**
      * @inheritDoc
      */
-    public function canDownload($objectID)
+    public function cacheObjects(array $objectIDs)
     {
-        // TODO
-        return true;
+        $objectList = new AssetList();
+        $objectList->setObjectIDs($objectIDs);
+        $objectList->readObjects();
+        $this->cachedObjects = $objectList->getObjects();
     }
 
     /**
      * @inheritDoc
      */
-    public function canViewPreview($objectID)
+    public function canDownload($objectID)
     {
-        return $this->canDownload($objectID);
+        return $this->getObject($objectID)->canView();
     }
 
     /**
@@ -28,8 +36,7 @@ class AssetAttachmentObjectType extends AbstractAttachmentObjectType
      */
     public function canUpload($objectID, $parentObjectID = 0)
     {
-        // TODO
-        return true;
+        return $this->getObject($objectID)->canModify();
     }
 
     /**
