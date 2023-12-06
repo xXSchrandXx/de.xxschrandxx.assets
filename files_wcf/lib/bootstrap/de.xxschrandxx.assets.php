d72c3a1cba13299b0daa5f7ed52067422035870b
xXSchrandXx <marvinschrand@yahoo.de>
2023-12-06 22:09:53 +0100

    Add PreloadPhrases

--- files_wcf/lib/bootstrap/de.xxschrandxx.assets.php
+++ files_wcf/lib/bootstrap/de.xxschrandxx.assets.php
@@ -1,10 +1,16 @@
 <?php
 
 use wcf\system\event\EventHandler;
+use wcf\system\language\preload\event\PreloadPhrasesCollecting;
 use wcf\system\worker\event\RebuildWorkerCollecting;
 
 return static function (): void {
     $eventHandler = EventHandler::getInstance();
+
+    // Preload phrases
+    $eventHandler->register(PreloadPhrasesCollecting::class, static function (PreloadPhrasesCollecting $event) {
+        $event->preload('wcf.dialog.confirmation.audit');
+    });
 
     // Delete qr codes
     $eventHandler->register(RebuildWorkerCollecting::class, static function (RebuildWorkerCollecting $event) {
